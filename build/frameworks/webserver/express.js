aae60508ea55808ef7c3f7cba601b2ee6465e132
sufiyanbmk <sufiyanbmk@gmail.com>
2023-05-29 04:21:42 +0530

    cors changed

--- build/frameworks/webserver/express.js
+++ build/frameworks/webserver/express.js
@@ -7,14 +7,14 @@ const express_1 = __importDefault(require("express"));
 const morgan_1 = __importDefault(require("morgan"));
 const cors_1 = __importDefault(require("cors"));
 const cookie_parser_1 = __importDefault(require("cookie-parser"));
-const config_1 = __importDefault(require("../../config"));
 const helmet_1 = __importDefault(require("helmet"));
 const expressConfig = (app) => {
     // Development logging
-    if (config_1.default.nodeEnv == 'development') {
-        app.use((0, morgan_1.default)('dev'));
-    }
-    app.use((0, cors_1.default)({ origin: "http://localhost:3000" }));
+    // if (configKeys.nodeEnv == 'development') {
+    app.use((0, morgan_1.default)('dev'));
+    // }
+    // app.use(cors({ origin: "http://localhost:3000" }));
+    app.use((0, cors_1.default)());
     app.use(express_1.default.json());
     app.use(express_1.default.urlencoded({ extended: true }));
     app.use((0, cookie_parser_1.default)());
